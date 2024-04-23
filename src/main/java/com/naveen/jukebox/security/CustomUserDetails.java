2cfd491f1a50afe06967345bf64cf6afa8e8949b
naveen kumar <naveenkumarerroju@gmail.com>
2024-04-23 16:08:01 +0530

    basic required classes

---
+++ src/main/java/com/naveen/jukebox/security/CustomUserDetails.java
@@ -0,0 +1,55 @@
+package com.naveen.jukebox.security;
+
+import com.naveen.jukebox.entity.UserEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final UserEntity user;
+
+    public CustomUserDetails(UserEntity user) {
+        super();
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(user.getRole().name());
+        return List.of(authority);
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
