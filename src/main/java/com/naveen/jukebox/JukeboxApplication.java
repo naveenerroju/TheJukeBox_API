86a35f79e948871eb498816f9fba86cb10dd6dea
Naveen Kumar <naveenkumarerroju@gmail.com>
2024-05-20 16:30:14 +0530

    added swagger documentation setup

--- src/main/java/com/naveen/jukebox/JukeboxApplication.java
+++ src/main/java/com/naveen/jukebox/JukeboxApplication.java
@@ -4,18 +4,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-/**
- * TODO:
- *      Add constructor parameters
- *      Make the requests return a valid response
- *      Document all the methods
- *      Users table should have all the playlists references
- *      Playlists table should have all the songs references
- *      Songs table should have all the playlists references
- *      Magic Strings and numbers
- */
+
 @SpringBootApplication
+@EnableWebMvc
 public class JukeboxApplication {
 
     public static void main(String[] args) {
@@ -27,7 +20,10 @@ public class JukeboxApplication {
      * <br>
      * <p>
      * Autowire the ModelMapper. Then use it to map the models.
-     * Example: <code>UserEntity entity = this.modelMapper.map(request, UserEntity.class);</code>
+     * Example: <code>
+     *     <br>
+     *     UserEntity entity = this.modelMapper.map(request, UserEntity.class);
+     * </code>
      * </p>
      *
      * @return the ModelMapper object
